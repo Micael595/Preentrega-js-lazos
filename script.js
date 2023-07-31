4d8cf240dae09df008e51d005eb8712d08578209
Micael595 <112912271+Micael595@users.noreply.github.com>
2023-07-31 19:41:41 -0300

    renovacion del index incluyendo localstorage y eventos

--- script.js
+++ script.js
@@ -1,60 +1,121 @@
-let pedido = prompt("Buenos dias cual va ser su pedido?  \n 1 simple con queso \n 2 doble con queso \n 3 doble roque \n 4 doble onion " )
+const menuElement = document.querySelector('#menu ul');
+const carritoElement = document.querySelector('#carrito ul');
+const totalElement = document.querySelector('#total');
+const vaciarCarritoButton = document.querySelector('#vaciar-carrito');
 
-let listaPedido = []
 
-function hamburguesa(nombre,carnes,quesos,precio,extra){
-    this.nombre = nombre
-    this.carnes = carnes
-    this.quesos = quesos
-    this.precio = precio
-    this.extra = extra
-}
-const simpleQueso = new hamburguesa("simple con queso","una carne","dos quesos",1600,"")
-const dobleQueso = new hamburguesa("doble con queso","dos carne","cuatro quesos",1800,"")
-const dobleRoque = new hamburguesa("doble roque","dos carne", "queso roquefort",1850,"")
-const dobleOnion = new hamburguesa("doble onion","dos carne","dos quesos",1900,"cebolla caramelizada")
-
-if (pedido == NaN ){
-    alert("Alerta, ha ingresado un valor no numerico, por favor ingrese un numero segun la hamburguesa deseada")
-}
-function totalPrice(precio,cantidad){
-    let price = (precio*cantidad)
-    return price
-}
-while (pedido != NaN) {
-    if (pedido == 1) {
-        let cantidad = prompt("cuantas simples con queso desea comprar? ")
-        let precioTotal = totalPrice(simpleQueso.precio,Number(cantidad))
-        alert ("usted ha comprado " + cantidad + " " + "simple con queso" + "a $"+ precioTotal + ", Muchas gracias por su compra")
-        listaPedido.push({pedido:simpleQueso.nombre,precio:precioTotal})
-    }else if (pedido == 2) {
-        let cantidad = prompt("cuantas doble con queso desea comprar?")
-        let precioTotal = totalPrice(dobleQueso.precio,Number(cantidad))
-        alert( "usted ha comprado " + cantidad + " " + "doble con Queso" + "a $" + precioTotal +", Muchas gracias por su compra")
-        listaPedido.push({pedido:dobleQueso.nombre,precio:precioTotal})
-    }else if (pedido == 3) {
-        let cantidad = prompt("cuantas doble roque desea comprar?")
-        let precioTotal = totalPrice(dobleRoque.precio,Number(cantidad))
-        alert( "usted ha comprado " + cantidad + " " + "doble Roque" + "a $" + precioTotal  + ", Muchas gracias por su compra")
-        listaPedido.push({pedido:dobleRoque.nombre,precio:precioTotal})
-    }else if (pedido == 4) {
-        let cantidad = prompt("cuantas doble onion desea comprar?")
-        let precioTotal = totalPrice(dobleOnion.precio,Number(cantidad))
-        alert( "usted ha comprado " + cantidad + " " + "doble Onion" + "a $" + precioTotal +", Muchas gracias por su compra")
-        listaPedido.push({pedido:dobleOnion.nombre,precio:precioTotal})
+const catalogo = [
+    {
+    id: 1,
+    nombre: 'Simple con queso',
+    precio: 1200
+    },
+    {
+    id: 2,
+    nombre: 'Doble con queso',
+    precio: 1500
+    },
+    {
+    id: 3,
+    nombre: 'Doble roque',
+    precio: 1750
+    },
+    {
+    id: 4,
+    nombre: 'Doble onion',
+    precio: 1800
     }
-    else if (pedido == 0 || pedido >= 5)
-    alert("Ha ingresado un numero fuera de las opciones disponibles, por favor reintentar ingresado 1,2,3 o 4")
-    break
+];
+
+    let carrito = [];
+    if (localStorage.getItem('carrito')) {
+    carrito = JSON.parse(localStorage.getItem('carrito'));
+    }
+
+    function agregarAlCarrito(id) {
+
+    const producto = catalogo.find(item => item.id === id);
+
+    producto.cantidad = 1;
+
+    carrito.push(producto);
+
+    actualizarCarrito();
 }
 
-alert("usted tiene un pedido de: " + JSON.stringify(Object.values(listaPedido)))
+    function eliminarDelCarrito(id) {
+    const index = carrito.findIndex(item => item.id === id);
+    carrito.splice(index, 1);
+    actualizarCarrito();
+}
 
-let nuevoPedido = prompt("Desea realizar otro pedido?, responder SI o NO" )
-    if ((nuevoPedido == "SI")|| (nuevoPedido == "si")){
-    location.reload()
-    }
-    else{
-        alert("Gracias por su compra en Burgerlab")
-    location.reload()
-    }
+    function vaciarCarrito() {
+    carrito = [];
+    actualizarCarrito();
+}
+
+    function actualizarCarrito() {
+    carritoElement.innerHTML = '';
+    carrito.forEach(producto => {
+
+    const li = document.createElement('li');
+
+    li.textContent = producto.nombre;
+
+    const boton = document.createElement('button');
+    boton.textContent = 'Eliminar';
+    boton.addEventListener('click', () => eliminarDelCarrito(producto.id));
+
+    li.appendChild(boton);
+    carritoElement.appendChild(li);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+  });
+
+        const total = carrito.reduce((suma, producto) => suma + producto.precio, 0);
+
+        totalElement.textContent = total.toFixed(2);
+}
+
+
+        function mostrarMenu() {
+        menuElement.innerHTML = '';
+
+        catalogo.forEach(producto => {
+
+        const li = document.createElement('li');
+        li.textContent = producto.nombre;
+
+        const boton = document.createElement('button');
+        boton.textContent = 'Agregar';
+        boton.addEventListener('click', () => agregarAlCarrito(producto.id));
+        li.appendChild(boton);
+        menuElement.appendChild(li);
+    });
+    } 
+
+    mostrarMenu(); 
+
+    vaciarCarritoButton.addEventListener('click', vaciarCarrito);
+
+
+const botonComprar = document.querySelector('#boton-comprar');
+botonComprar.addEventListener('click', () => {
+    let total = 0;
+    carrito.forEach(producto => {
+    total += producto.precio * producto.cantidad;
+    });
+    const mensaje = `¡Gracias por tu compra! El total es: $${total.toFixed(2)}`;
+    alert(mensaje);
+});
+
+document.body.style.backgroundColor = "#f2f2f2";
+document.body.style.fontFamily = "Arial, sans-serif";
+document.body.style.textAlign = "center";
+document.body.style.alignItems= "center";
+
+const headers = document.querySelectorAll('h1');
+headers.forEach(header => {
+header.style.fontSize = "40px";
+header.style.height="30px";
+
+});
